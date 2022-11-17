8cb6f9a94dd9b8ee6cfdec81bd963f3f23dc8ddb
Shimil <shimiltharayil007@gmail.com>
2022-11-17 10:41:35 +0530

    Initial Commit

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header className='sticky shadow-lg top-0' >
+        <div className='pl-10 pt-3 pb-3 md:pt-5 md:pb-5'>
+            <img className='cursor-pointer' src="https://deepthought.education/assets/images/logo/logo.svg" alt="" />
+        </div>
+    </header>
+  )
+}
